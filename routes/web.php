2e8f9135123c553ad967dae1d96766aec3d4518a
UltimateCoder-Sohail <sohailakhtar11sci@gmail.com>
2025-07-02 20:34:40 +0530

    .

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminDashboardController;
 use App\Http\Middleware\isAdmin;
 
 Route::get('/', function () {
@@ -20,18 +21,6 @@ Route::get('/contact', function () {
 Route::get('/privacy-policy', function () {
     return view('privacy-policy');
 });
-
-Route::prefix('admin')->name('admin.')->middleware(['auth', 'is_admin'])->group(function () {
-    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-
-    // Example CRUD routes
-    Route::resource('users', AdminUserController::class);
-    Route::resource('settings', AdminSettingController::class);
-});
-// routes/web.php
-Route::get('auth/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('auth/login', [AuthController::class, 'login']);
-Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::get('/check-flights', [FlightController::class, 'check'])->name('check.flights');
