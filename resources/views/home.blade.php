5ba997f48d334b118abb04c1fdbc197cccf7686e
Mohd Masood Alam <130masoodalam@gmail.com>
2025-07-06 17:35:47 +0530

    added fix to work with tailwindcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sasta Tickets</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <link rel="stylesheet" href="css/homepage.css">
     <!--
 
--- resources/views/hotels.blade.php
+++ resources/views/hotels.blade.php
@@ -1,6 +1,6 @@
 <div class="hotels-container">
     <h2>Best Hotel Deals</h2>
-    <div class="city-grid">
+    <div class="city-grid grid grid-cols-3">
         @foreach ($cities as $city)
             <div class="city">
                 <img src={{$city['image']}} alt={{$city['name']}} >
