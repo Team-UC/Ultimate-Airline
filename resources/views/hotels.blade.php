8e59512228f4d81760bd2dda0854e1c6f0e64de1
Aalia jabeen <aaliajabeen909@gmail.com>
2025-07-06 21:03:22 +0530

    added css for the best hotel deals

--- resources/views/hotels.blade.php
+++ resources/views/hotels.blade.php
@@ -1,11 +1,11 @@
 <div class="hotels-container">
-    <h2>Best Hotel Deals</h2>
+    <h2 class="hotels-deal-title">Best Hotel Deals</h2>
     <div class="city-grid">
         @foreach ($cities as $city)
             <div class="city">
                 <img src={{$city['image']}} alt={{$city['name']}} >
-                <p class="city-name"> {{$city['name']}} </p>
-                <p class="country-name"> {{$city['country']}} </p>
+                <p class="city-name "> {{$city['name']}} </p>
+                <p class="city-country"> {{$city['country']}} </p>
             </div>
         @endforeach
     </div>
