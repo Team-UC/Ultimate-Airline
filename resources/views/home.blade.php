cfe28fea7d924129c3b8ca7af50b845899666f36
UltimateCoder-Sohail <sohailakhtar11sci@gmail.com>
2025-08-14 00:33:08 +0530

    ci/cd demo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -504,7 +504,7 @@ https://www.tooplate.com/view/2095-level
             <div class="row text-center">
                 <article class="col-sm-12 col-md-4 col-lg-4 col-xl-4 tm-article">
                     <i class="fa tm-fa-6x fa-legal tm-color-primary tm-margin-b-20"></i>
-                    <h3 class="tm-color-primary tm-article-title-1">Level HTML Template by Tooplate website</h3>
+                    <h3 class="tm-color-primary tm-article-title-1">Abuzar Rustam HTML Template by Tooplate website</h3>
                     <p>You are allowed to download, edit and use this template for your business or client websites.</p>
                     <a href="#" class="text-uppercase tm-color-primary tm-font-semibold">Continue reading...</a>
                 </article>
